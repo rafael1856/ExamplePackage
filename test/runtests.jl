57a5f69351211be03cfdf47ef3021c3850aae543
rafael1856 <racostacuriel@gmail.com>
2019-07-11 02:07:47 +0000

    learning

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using ExamplePackage
 using Test
 
 @testset "ExamplePackage.jl" begin
-    # Write your own tests here.
+  # Write your own tests here.
+  @test foo() ≈ 0.11388071406436832
+  @test foo(1, 1.5) ≈ 0.2731856314283442
+  @test_broken foo(1, 0) # tells us this is broken
 end
